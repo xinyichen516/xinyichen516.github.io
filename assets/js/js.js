fb2dbcbf34033e9ba3533823bd3a72bd96ff1ccf
xinyichen516 <xinyichen@berkeley.edu>
2017-04-25 19:00:33 -0700

    almost done

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,3 +1,17 @@
 (function blink() { 
   $('.blink').fadeOut(500).fadeIn(500, blink); 
 })();
+
+$(function(){
+   // See if this is a touch device
+   if ('ontouchstart' in window)
+   {
+      // Set the correct [touchscreen] body class
+      $('body').removeClass('no-touch').addClass('touch');
+     
+      // Add the touch toggle to show text when tapped
+      $('div.boxInner img').click(function(){
+         $(this).closest('.boxInner').toggleClass('touchFocus');
+      });
+   }
+});
