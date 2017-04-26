721d6971942681217731c2b2153133676f386d08
xinyichen516 <xinyichen@berkeley.edu>
2017-04-25 23:20:01 -0700

    finished!

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,3 +1,9 @@
 (function blink() { 
   $('.blink').fadeOut(500).fadeIn(500, blink); 
 })();
+
+$(document).ready(function(){
+    $(".submit").click(function(){
+        alert("Thank you for the message! I usually respond within 48 hours.");
+    });
+});
